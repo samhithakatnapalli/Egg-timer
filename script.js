8da3e77e15e0ee8cd79c6a29de26041849eccbc4
Samhitha Katnapalli <103943366+samhithakatnapalli@users.noreply.github.com>
2025-07-21 12:23:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-let intervalId;
-
 function startTimer() {
   let timeLeft = 10;
-  const timerDisplay = document.getElementById("timerDisplay");
+  const timerDisplay = document.getElementById("timer");
   const imageDisplay = document.getElementById("eggImage");
   const startBtn = document.getElementById("startBtn");
+
   imageDisplay.src = "first step.png";
   timerDisplay.textContent = `Time: ${timeLeft}s`;
   startBtn.disabled = true;
 
-  startBtn.style.display="none";
+  setTimeout(() => {
+    imageDisplay.src = "fried-egg-pan.gif";
+  }, 1000);
 
-  clearInterval(intervalId);
-    intervalId = setInterval(() => {
-        timeLeft--;
-        if (timeLeft >= 0) {
-            timerDisplay.textContent = timeLeft + "s";
-        }
-        else {
-            clearInterval(intervalId);
-            timerDisplay.textContent = "Done!";
-            image.src = "final step.png";
-            startBtn.disabled= false;
-        }
-    }, 1000);
+  const timer = setInterval(() => {
+    timeLeft--;
+    timerDisplay.textContent = `Time: ${timeLeft}s`;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      timerDisplay.textContent = "Done! 🍳";
+      imageDisplay.src = "final step.png";
+      startBtn.disabled = false;
+    }
+  }, 1000);
 }
